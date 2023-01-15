9b3f8ed9374c69b767575efcbbc632c78d9141f6
Kyle Galway <galwayk@sheridancollege.ca>
2023-01-15 16:17:12 -0500

    Add two columns rendering the demonstration app titles.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="centered", page_title="About Me")
 
@@ -7,7 +8,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.write("<br><br>", unsafe_allow_html=True)
     image_filepath = "files/images/photo.png"
-    st.image(image_filepath)
+    st.image(image_filepath, use_column_width=False)
 
 with col2:
     st.title("Who am I?")
@@ -21,3 +22,14 @@ with col2:
     st.info(creator_info_text)
 
 st.write("Some projects I have completed include the following: ")
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("files/data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
